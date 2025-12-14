0165c44b9c8fadfcc7e35dc879a3bd6817de7db6
Andrew <me@andrewwhaddad.com>
2025-12-14 07:33:11 -0500

    Fix database migration CI by skipping rollback test for DDL migrations
    - Disable rollback testing in ci-database.yml since DDL migrations with triggers/functions are not easily reversible
    - Add DROP FUNCTION to accounts migration down.sql to clean up shared function

--- migrations/20251213144059_create_accounts_table.sql
+++ migrations/20251213144059_create_accounts_table.sql
@@ -39,3 +39,4 @@ DROP INDEX IF EXISTS idx_accounts_created_at;
 DROP INDEX IF EXISTS idx_accounts_email;
 DROP INDEX IF EXISTS idx_accounts_username;
 DROP TABLE IF EXISTS accounts;
+DROP FUNCTION IF EXISTS update_updated_at_column();
